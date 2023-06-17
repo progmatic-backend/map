0dcd1e78994bdc9e7db86297acdbf14ec346e4c9
vasandris01 <undris01@gmail.com>
2023-06-17 11:02:26 +0200

    feladat

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
 public class Main {
+    public static void main(String[] args) {
+        //írj egy Mapet ahol string kulcsokhoz string értékeket párosítasz
+
+        // töltsd fel a hónapokkal és hogy milyen évszakhoz tartoznak
+
+        // írj egy mapet ahol string kulcsokhoz string lista tartozik
+
+        // töltsd fel évszakok-hónapokkal
+
+        //szorgalmi
+        // írj egy mapet ahol Integer kulcsokhoz Integer értékeket tárolsz
+        // modelez 1000 kockadobást (1-6 random szám)
+        // a dobásokat tárold el a mapben 1 es dobás kulcs -> hányszorvolt
+    }
 }
